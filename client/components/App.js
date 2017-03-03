32bdb0675e3833c33d97b72f280c6f62b96d338d
Emir Demirdag <emirdemirdag@gmail.com>
2017-03-03 18:03:00 +0100

    Setup initial project

---
+++ client/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = React.createClass({
+  render () {
+    return (
+      <div id='app'>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+})
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+  context: __dirname,
+  entry: './client/index.js',
+  devtool: 'source-map',
+  output: {
+    path: path.join(__dirname, '/public'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.json']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        include: path.resolve(__dirname, 'client'),
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
