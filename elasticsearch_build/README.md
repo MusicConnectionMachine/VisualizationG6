7bfd9876b1284c37f21e817578d1cdfb1f8ca1c5
Markus Sosnowski <markus.sosnowski@tum.de>
2017-03-10 11:55:26 +0100

    Loading sample data directly at docker build time

--- elasticsearch_build/README.md
+++ elasticsearch_build/README.md
@@ -2,6 +2,6 @@
 
 The data dir of Elasticseach shoul be mounted externally in a persistend environment! This setup is only for early development.
 
-To build the custom image, extract the zip and the `#` before **build** in `docker-compose.yml` has to be removed.
+To build the custom image, the `#` before **build** in `docker-compose.yml` has to be removed. Run `docker-compose build --no-cache`
 
 Then the image can be uploaded with `docker login` and `docker push musicconnectionmachine/elasticsearch-testdata:custom-tag`
--- elasticsearch_build/startup.sh
+++ elasticsearch_build/startup.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-# No permission for this..
-if [[ -e /usr/share/elasticsearch/data/initialized ]]
-then
-	exit 0
-fi
+# This File is executed one at build time from use elasticsearch
 
-while ! curl http://127.0.0.1:9200; do sleep 1; done;
+# The normal entrypoint of elasticsearch - thus will start the server
+bin/es-docker &
 
-curl -XPOST -u elastic:changeme 'localhost:9200/_xpack/security/role/read_music' -d '{ "indices" : [ { "names" : [ "music*" ], "privileges" : [ "read" ] } ] }'
+# Wait for the server to come up
+while ! curl http://127.0.0.1:9200; do sleep 3; done;
 
-curl -XPOST -u elastic:changeme 'localhost:9200/music/composition/_bulk?pretty&refresh' --data-binary "@/usr/local/bin/composition-test-data.json" -H 'Content-Type: application/json'
+# Set the read_music role
+curl -XPOST -u elastic:changeme 'localhost:9200/_xpack/security/role/read_music' -d '
+	{ "indices" : 
+		[ 
+			{ 
+				"names" : [ "music*" ], 
+				"privileges" : [ "read" ] 
+			} 
+		]
+	}'
 
-touch /usr/local/bin/initialized
+# Load samle data
+curl -XPOST -u elastic:changeme 'localhost:9200/music/composition/_bulk?refresh' --data-binary "@/usr/local/bin/composition-test-data.json" -H 'Content-Type: application/json'
