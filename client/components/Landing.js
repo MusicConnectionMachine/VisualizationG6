bf6a75cce00b6e59747801622a500872d7614e17
Emir Demirdag <emirdemirdag@gmail.com>
2017-03-19 22:14:15 +0100

    Minor fix on alt of img tag

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -11,7 +11,7 @@ const Landing = React.createClass({
       <div className='home-background--patterns full-height'>
         <div className='container'>
           <div className='center-block text-center text-white margin-top-10-p animated fadeIn'>
-            <img src='/style/img/logo-landing-white.svg' alt='Kiwi standing on oval' />
+            <img src='/style/img/logo-landing-white.svg' alt='Music Connection Machine Logo' />
           </div>
           <div className='row margin-top-20-p'>
             <Searchbar />
