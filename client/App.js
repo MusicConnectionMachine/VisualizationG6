efe5bb909d443686982f1d7f7f75d90193bc838c
Emir Demirdag <emirdemirdag@gmail.com>
2017-03-29 01:58:55 +0200

    Add main redux files

--- client/App.js
+++ client/App.js
@@ -4,6 +4,8 @@ import Landing from './components/Landing'
 import Search from './components/Search'
 import DetailPage from './components/singleentry/DetailPage'
 import { BrowserRouter, Match } from 'react-router'
+import { Provider } from 'react-redux'
+import store from './states/store'
 import 'bootstrap/dist/css/bootstrap.css'
 import '../style/index.scss'
 
@@ -11,19 +13,21 @@ const App = React.createClass({
   render () {
     return (
       <BrowserRouter>
-        <div className='app'>
-          <Match exactly pattern='/' component={Landing} />
-          <Match pattern='/details/:type/:id'
-            component={(props) => {
-              return <DetailPage type={props.params.type} id={props.params.id} />
-            }} />
-          <Match
-            pattern='/search/:query'
-            component={(props) => {
-              return <Search searchTerm={props.params.query} />
-            }}
-          />
-        </div>
+        <Provider store={store}>
+          <div className='app'>
+            <Match exactly pattern='/' component={Landing} />
+            <Match pattern='/details/:type/:id'
+              component={(props) => {
+                return <DetailPage type={props.params.type} id={props.params.id} />
+              }} />
+            <Match
+              pattern='/search/:query'
+              component={(props) => {
+                return <Search searchTerm={props.params.query} />
+              }}
+            />
+          </div>
+        </Provider>
       </BrowserRouter>
     )
   }
--- client/components/Searchbar.js
+++ client/components/Searchbar.js
@@ -1,35 +1,37 @@
 import React from 'react'
+import { connect } from 'react-redux'
 const { object, string } = React.PropTypes
 
 const Searchbar = React.createClass({
   propTypes: {
-    initSearchTerm: string
+    searchTerm: string
   },
   contextTypes: {
     router: object
-  },
-  getInitialState () {
-    return {
-      searchTerm: this.props.initSearchTerm
-    }
   },
   handleSearchTermChange (event) {
     this.setState({searchTerm: event.target.value})
   },
   handleSearchSubmit (event) {
     event.preventDefault()
-    this.context.router.transitionTo(`/search/${this.state.searchTerm}`)
+    this.context.router.transitionTo(`/search/${this.props.searchTerm}`)
   },
   render () {
     return (
       <div className='parent-center searchbar animated fadeIn'>
         <form onSubmit={this.handleSearchSubmit}>
           <input onChange={this.handleSearchTermChange} type='search' className='form-control'
-            placeholder='Type your favorite musician.' value={this.state.searchTerm} />
+            placeholder='Type your favorite musician.' value={this.props.searchTerm} />
         </form>
       </div>
     )
   }
 })
 
-export default Searchbar
+const mapStateToProps = (state) => {
+  return {
+    searchTerm: state.searchTerm
+  }
+}
+
+export default connect(mapStateToProps)(Searchbar)
