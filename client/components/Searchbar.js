4dc8400647e33c35860d38ac84c09166595e222e
syncall <markus.sosnowski@tum.de>
2017-04-10 20:01:10 +0200

    Implement Autocomplete
    The Code is partly copied from Group 5

--- client/components/Searchbar.js
+++ client/components/Searchbar.js
@@ -1,34 +1,82 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { setSearchTerm } from '../states/actionCreators'
+import Autocomplete from 'react-autocomplete'
 const { object, string, func } = React.PropTypes
 
-const Searchbar = React.createClass({
-  propTypes: {
-    searchTerm: string,
-    dispatch: func
-  },
-  contextTypes: {
-    router: object
-  },
+class Searchbar extends React.Component {
+
+  constructor (props) {
+    super(props)
+
+    this.handleSearchSubmit = this.handleSearchSubmit.bind(this)
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this)
+
+    this.state = {
+      suggestions: [ 'Mozart', 'Beethoven', 'Leopold Mozart', 'father of Mozart' ],
+      searchTerm: props.searchTerm
+    }
+  }
+
   handleSearchTermChange (event) {
-    this.props.dispatch(setSearchTerm(event.target.value))
-  },
+    let value = event.target.value
+    this.setState({searchTerm: value})
+    this.props.dispatch(setSearchTerm(value))
+    // TODO query Database for suggestions
+  }
+
   handleSearchSubmit (event) {
     event.preventDefault()
+
     this.context.router.transitionTo('/search')
-  },
+  }
+
+  getItemValue (item) {
+    return item
+  }
+
   render () {
     return (
       <div className='parent-center searchbar animated fadeIn'>
         <form onSubmit={this.handleSearchSubmit}>
-          <input onChange={this.handleSearchTermChange} type='search' className='form-control'
-            placeholder='Type your favorite musician.' value={this.props.searchTerm} />
+          <Autocomplete
+            getItemValue={this.getItemValue}
+            inputProps={{
+              type: 'search',
+              className: 'form-control',
+              placeholder: 'Type your favorite musician.'
+            }}
+            items={this.state.suggestions}
+            onChange={this.handleSearchTermChange}
+            onSelect={(value) => {
+              this.setState({searchTerm: value})
+              this.props.dispatch(setSearchTerm(value))
+            }}
+            renderItem={(item, isHighlighted) => (
+              <div className={'list-group-item' + (isHighlighted ? ' active' : '')}>
+                { item }
+              </div>
+            )}
+            renderMenu={(items, value, style) => (
+              <div className='list-group' children={items} />
+            )}
+            wrapperStyle={{}}
+            value={this.state.searchTerm}
+          />
         </form>
       </div>
     )
   }
-})
+}
+
+Searchbar.propTypes = {
+  searchTerm: string,
+  dispatch: func
+}
+
+Searchbar.contextTypes = {
+  router: object
+}
 
 const mapStateToProps = (state) => {
   return {
