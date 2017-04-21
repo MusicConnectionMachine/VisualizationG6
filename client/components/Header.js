4bb08ceeb73340a2a4f3d784c5e8c1963f15d303
Emir Demirdag <emirdemirdag@gmail.com>
2017-04-21 23:07:10 +0200

    Add styles to header

--- client/components/Header.js
+++ client/components/Header.js
@@ -12,16 +12,8 @@ class Header extends React.Component {
     }
     return (
       <nav className='navbar navbar-default navbar-fixed-top'>
-        <div className='container-fluid tumheader'>
-          <div className='navbar-header'>
-            <div className='tumtext'>Javascript technology 2017 Seminar<br />Technical University of Munich</div>
-          </div>
-          <div className='pull-right'>
-            <a href='http://www.tum.de' target='_blank'><img className='' src={TumLogo} /></a>
-          </div>
-        </div>
-        <div className='container-fluid navigation'>
-          <div className='navbar-header'>
+        <div className='container-fluid navigation row parent-center' style={{ padding: '0px 20px' }}>
+          <div className='navbar-header col-md-4'>
             <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar'>
               <span className='icon-bar' />
               <span className='icon-bar' />
@@ -31,8 +23,13 @@ class Header extends React.Component {
               <h2>MusicConnectionMachine</h2>
             </Link>
           </div>
-          <div className='collapse navbar-collapse' id='navbar'>
-            <ul className='navbar-nav'>
+          <div className='col-md-4 parent-center'>
+            <a href='http://www.tum.de' target='_blank'>
+              <img className='tum-logo' src={TumLogo} />
+            </a>
+          </div>
+          <div className='col-md-4 parent-center' id='navbar'>
+            <ul className='navbar-nav pull-right nav-menu'>
               <li className='parent-center'><a href='http://54.68.164.199:8083' target='_blank'>Plugins</a></li>
               <li className='parent-center'><a href='https://github.com/MusicConnectionMachine/api' target='_blank'>API</a></li>
               <li className={(location.indexOf('about') > 0 ? 'selected' : '') + ' parent-center'}><Link className='' to='/about'>About</Link></li>
