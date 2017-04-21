a8409d3b625fff06deeaf08e0125f3da0db26b2d
Emir Demirdag <emirdemirdag@gmail.com>
2017-04-21 21:32:42 +0200

    Show header on the landing page

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Header from './Header'
 import Searchbar from './Searchbar'
 import headerImage from '../../style/img/logo-landing-white.svg'
 import '../../style/landing.scss'
@@ -6,12 +7,15 @@ import '../../style/landing.scss'
 const Landing = React.createClass({
   render () {
     return (
-      <div className='container-fluid'>
-        <div className='center-block text-center text-white margin-top-10-p animated fadeIn'>
-          <img src={headerImage} alt='Music Connection Machine Logo' />
-        </div>
-        <div className='row margin-top-20-p'>
-          <Searchbar />
+      <div>
+        <Header />
+        <div className='container-fluid'>
+          <div className='center-block text-center text-white margin-top-10-p animated fadeIn'>
+            <img src={headerImage} alt='Music Connection Machine Logo' />
+          </div>
+          <div className='row margin-top-20-p'>
+            <Searchbar />
+          </div>
         </div>
       </div>
     )
