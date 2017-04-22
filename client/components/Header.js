aa1c0f88c094b561938a135a52b011f4bf40fe2b
syncall <markus.sosnowski@tum.de>
2017-04-22 17:20:27 +0200

    Some changes

--- client/components/Header.js
+++ client/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router'
 import Searchbar from './Searchbar'
 import TumLogo from '../../style/img/TUM_Web_Logo_neg.svg'
+import 'bootstrap/js/collapse'
 
 class Header extends React.Component {
   render () {
@@ -9,15 +10,13 @@ class Header extends React.Component {
     let utilSpace, colClass
     if (location !== '/') {
       utilSpace = (
-        <div className='col-md-4 parent-center'>
-          <div className='navbar-form navbar-right'>
-            <Searchbar />
-          </div>
+        <div className='navbar-form navbar-right'>
+          <Searchbar />
         </div>
       )
       colClass = ''
     } else {
-      colClass = 'pull-right'
+      colClass = ''
     }
     return (
       <nav className='navbar navbar-default navbar-fixed-top'>
@@ -29,8 +28,8 @@ class Header extends React.Component {
             </a>
           </div>
         </div>
-        <div className='container-fluid navigation row parent-center' style={{ padding: '0px 20px' }}>
-          <div className='navbar-header col-md-4'>
+        <div className='container-fluid navigation'>
+          <div className='navbar-header'>
             <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar'>
               <span className='icon-bar' />
               <span className='icon-bar' />
@@ -40,14 +39,14 @@ class Header extends React.Component {
               <h2>MusicConnectionMachine</h2>
             </Link>
           </div>
-          <div className={'col-md-4 parent-center ' + colClass} id='navbar'>
-            <ul className='navbar-nav pull-right nav-menu'>
-              <li className='parent-center'><a href='http://54.68.164.199:8083' target='_blank'>Plugins</a></li>
-              <li className='parent-center'><a href='https://github.com/MusicConnectionMachine/api' target='_blank'>API</a></li>
-              <li className={(location.indexOf('about') > 0 ? 'selected' : '') + ' parent-center'}><Link className='' to='/about'>About</Link></li>
+          <div className={'collapse navbar-collapse ' + colClass} id='navbar'>
+            <ul className='nav navbar-nav nav-menu'>
+              <li className=''><a href='http://54.68.164.199:8083' target='_blank'>Plugins</a></li>
+              <li className=''><a href='https://github.com/MusicConnectionMachine/api' target='_blank'>API</a></li>
+              <li className={(location.indexOf('about') > 0 ? 'active' : '')}><Link className='' to='/about'>About</Link></li>
             </ul>
+            {utilSpace}
           </div>
-          {utilSpace}
         </div>
       </nav>
     )
