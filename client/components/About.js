75a9638cf68ee60decc3b1d5e60a3c4c26dd69c8
DESKTOP-GHSSEBC\Yeti <getire@gmail.com>
2017-04-22 19:08:28 +0200

    Those are the static pages, worked locally.

--- client/components/About.js
+++ client/components/About.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Header from './Header'
-import groupPhoto from '../../style/img/groupPhoto.jpg'
 import '../../style/about.scss'
 
 class About extends React.Component {
@@ -9,17 +8,27 @@ class About extends React.Component {
       <div className='about'>
         <Header />
         <div className='container-fluid animated fadeIn'>
-          <img className='group-photo' src={groupPhoto} />
-          <div className='row' style={{marginTop: '20px'}}>
+         <div className='row' style={{marginTop: '20px'}}>
             <div className='col-md-12 text-center'>
-              <h3>About the Project</h3>
-              <p className='text-left'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus,
-                itaque, est expedita error aliquid deleniti
-                voluptatem ducimus inventore labore autem beatae molestiae. Deserunt nesciunt dolorum maiores illum cum,
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, obcaecati! Quibusdam, voluptate, nulla. Vitae aspernatur amet, modi neque, quis tempore. Labore sunt consectetur odio nemo sint adipisci quam nihil eaque.
-                doloribus laboriosam.
-              </p>
+              <h1>About</h1>
             </div>
+           <div className='col-md-5 col-sm-10 col-sm-offset-1'>
+             <img src={'http://img.wennermedia.com/social/rs-game-of-thrones-8e3695f2-f54d-40e0-9cf6-bddbe5340c11.jpg'} height='500' width='450'/>
+           </div>
+           <div className='col-md-5 col-md-offset-0 col-sm-10 col-sm-offset-1'>
+             <p className='text-left'>Our main focus is to:
+                 <li>Provide a summary of the Internet’s knowledge about classical music.</li>
+                 <li>Provide an engine that will index and allow everyone to find both facts and opinion about people and works of classical music.</li>
+                 <li>Provide a mapping between composers, artists and classical music works.</li>
+                 <li>Provide visual tools to navigate these mappings, letting end users explore the data universe by defining their own search terms and context.</li>
+                 <li>Create a map of the places on the Internet that contain information about classical music.</li>
+                 <li>Provide users to contribute more knowledge → crowd source a la wikipedia. (optional)</li>
+
+               Use cases:
+               <li>If one person is claimed to have said something about another person or work, we want to have this quote accessible through our system.</li>
+               <li>If people or works have been ascribed certain attributes, we want to collect and aggregate them and present the sources of these claims to our users.</li>
+               <li>The map of places on the internet that contain information about classical music will be useful to any follow-up project that wants to pursue similar research without having to scan the CommonCrawl dataset again and create the filtering procedure from scratch.</li>             </p>
+           </div>
           </div>
           <div className='row' style={{marginTop: '20px'}}>
             <div className='col-md-12 text-center'>
--- client/components/static/Imprint.js
+++ client/components/static/Imprint.js
@@ -1,35 +1,39 @@
+/**
+ * Created by Yeti on 19-Apr-17.
+ */
 import React from 'react'
 
 class Imprint extends React.Component {
   render () {
     return (
       <div className='container-fluid animated fadeIn'>
-        <h2>Imprint & Disclaimer</h2>
-        <h3>Imprint</h3>
-        <h4>Address</h4>
-        <address>
-          <strong>Technische Universität München</strong><br />
-          Arcisstraße 21<br />
-          80333 München<br />
-          <abbr title='Phone'>Telefon:</abbr> +49 89 289-01
-        </address>
-
-        <h3>Authorised representativey</h3>
-        <p>The Technische Universität München is a public body. It is legally represented by the President, Prof. Dr. Dr. H. C. mult. Wolfgang A. Herrmann.</p>
-
-        <h4>Supervisory authority</h4>
-        <p>Bavarian Ministry of Education and Culture, Science and Art</p>
-
-        <h4>Content responsibility</h4>
-        <address>
-          <strong>Dr. Guy Yachdav</strong><br />
-          Arcisstr. 19<br />
-          80333 München<br />
-          E-Mail: gyachdav@rostlab.org
-        </address>
-
-        <h3>Liability notice</h3>
-        <p>Despite careful content control we assume no liability for the content of external links. For the corresponding content of linked pages, their administrators are responsible. Especially marked contributions in the discussion areas represent the opinion of the author. For the contents of the articles, the authors are solely responsible.</p>
+        <div>
+          <h1>Imprint</h1>
+          <h3>Address</h3>
+          <p>Technische Universität München</p>
+          <p>Arcisstraße 21, 80333 München</p>
+          <p>Telefon: +49 89 289-01</p>
+        </div>
+        <div>
+          <h3>Authorised representative:</h3>
+          <p>The Technische Universität München is a public body. It is legally represented by the President, Prof. Dr. Dr. H. C. mult. Wolfgang A. Herrmann.</p>
+        </div>
+        <div>
+          <h3>Supervisory authority:</h3>
+          <p>Bavarian Ministry of Education and Culture, Science and Art</p>
+        </div>
+        <div>
+          <h3>Address</h3>
+          <p>Dr. Guy Yachdav</p>
+          <p>Arcisstr. 19</p>
+          <p>80333 München</p>
+          <p>E-Mail:<a href='mailto:gyachdav@rostlab.org'>gyachdav@rostlab.org</a></p>
+        </div>
+        <div>
+          <h3>Liability notice</h3>
+          <p>Despite careful content control we assume no liability for the content of external links. For the corresponding content of linked pages, their administrators are responsible. </p>
+          <p>Especially marked contributions in the discussion areas represent the opinion of the author. For the contents of the articles, the authors are solely responsible.</p>
+        </div>
       </div>
     )
   }
