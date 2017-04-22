d2573c33947edee67d7061d605d66320d916f945
Emir Demirdag <emirdemirdag@gmail.com>
2017-04-22 14:39:46 +0200

    Create about page

--- client/App.js
+++ client/App.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom'
 import Bundle from './Bundle'
 import { BrowserRouter, Match } from 'react-router'
 import { Provider } from 'react-redux'
-import Footer from './components/Footer'
 import store from './states/store'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/js/collapse'
@@ -31,7 +30,6 @@ const App = React.createClass({
             <Match exactly pattern='/legal' component={() => {
               return <Bundle load={() => import('./components/Legal')} />
             }} />
-            <Footer />
           </div>
         </Provider>
       </BrowserRouter>
