28923edc3b2a359d202a676019e3910875926bb8
Emir Demirdag <emirdemirdag@gmail.com>
2017-04-23 17:12:24 +0200

    Add initial widget section to start page

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Searchbar from './Searchbar'
 import Header from './Header'
+import LandingWidgets from './static/LandingWidgets'
 import headerImage from '../../style/img/logo/logo-landing.svg'
 import '../../style/landing.scss'
 
@@ -17,6 +18,7 @@ const Landing = React.createClass({
             <Searchbar displayHelpText />
           </div>
         </div>
+        <LandingWidgets />
       </div>
     )
   }
---
+++ client/components/static/LandingWidgets.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import timelinePhoto from '../../../style/img/timeline.png'
+const LandingWidgets = React.createClass({
+  render () {
+    return (
+      <section id='widgets' className='dark-bg container-fluid light-typo padding-top-bottom'>
+        <div className='container'>
+          <header className='section-header text-center'>
+            <h1 className='scrollimation scale-in'>Widgets</h1>
+          </header>
+          <div className='row'>
+            <div className='col-sm-8 col-sm-offset-2'>
+              <img className='img-responsive img-center scrollimation fade-left' src={timelinePhoto} alt='' />
+              <p className='text-center scrollimation fade-in'>I'm curre</p>
+            </div>
+          </div>
+          <p className='text-center'><a className='btn btn-meflat scrollto white icon-left' href='#contact'><i className='fa fa-arrow-down' />Hire Me</a></p>
+        </div>
+      </section>
+    )
+  }
+})
+
+export default LandingWidgets
