fd899556d6b43414352706afa6de42569e4fd58d
DESKTOP-7285AU5\Joern von Henning <joern.von.henning@gmail.com>
2017-04-23 12:56:42 +0200

    About Site. Text Description ToDO

--- client/components/About.js
+++ client/components/About.js
@@ -22,30 +22,142 @@ class About extends React.Component {
             </div>
           </div>
           <div className='row' style={{marginTop: '20px'}}>
-            <div className='col-md-12 text-center'>
-              <h3>Members</h3>
-              <ul className='students'>
-                <div className='row'>
-                  <div className='col-md-6 text-right'>
-                    <li>Lorem</li>
-                    <li>Lorem</li>
-                    <li>Lorem</li>
-                    <li>Lorem</li>
-                    <li>Lorem</li>
-                    <li>Lorem</li>
-                    <li>Lorem</li>
+            <div className='col-md-12'>
+              <h3 className='text-center'>Contributors</h3>
+              <div className='row text-center'>
+                <div className='col-md-offset-1 col-md-10'>
+                  <div className='contributors col-md-6'>
+                    <h4>Mentors</h4>
+                    <ul>
+                      <li><a href='https://github.com/gyachdav'>Guy Yachdav</a></li>
+                      <li><a href='https://github.com/sacdallago'>Christian Dallago</a></li>
+                      <li><a href='https://bruck.me'>Kordan Bruck</a></li>
+                      <li><a href='https://github.com/vviro'>Vladimir</a></li>
+                      <li><a href='https://github.com/pfent'>Philipp Fent</a></li>
+                      <li><a href='https://github.com/goldbergtatyana'>Tatyana Goldberg</a></li>
+                    </ul>
                   </div>
-                  <div className='col-md-6 text-left'>
-                    <li>Lorem</li>
-                    <li>Lorem</li>
-                    <li>Lorem</li>
-                    <li>Lorem</li>
-                    <li>Lorem</li>
-                    <li>Lorem</li>
-                    <li>Lorem</li>
+                  <div className='contributors col-md-6'>
+                    <h4>Data Aggregation</h4>
+                    <ul>
+                      <li><a href='https://github.com/LukasNavickas'>Lukas Navickas</a></li>
+                      <li><a href='https://github.com/ShilpaGhanashyamGore'>Shilpa Ghanashyam Gore</a></li>
+                      <li><a href='https://github.com/angelinrashmi2000'>Angelin Rashmi</a></li>
+                      <li><a href='https://github.com/TimHenkelmann'>Tim Henkelmann</a></li>
+                      <li><a href='https://github.com/lgfrbcsgo'>Felix Schorer</a></li>
+                      <li><a href='https://github.com/anshul0708'>Anshul Sharma</a></li>
+                      <li><a href='https://github.com/nyxathid'>Nikita Basargin</a></li>
+                      <li><a href='https://github.com/lukasstreit'>Lukas Streit</a></li>
+                    </ul>
+                  </div>
+                  <div className='contributors col-md-6'>
+                    <h4>Defining Relationships</h4>
+                    <ul>
+                      <li><a href='https://github.com/krishenk'>Krishen Kant Kandwal</a></li>
+                      <li><a href='https://github.com/ansjin'>Anshul Jindal</a></li>
+                      <li><a href='https://github.com/Henni'>Hendrik Leppelsack</a></li>
+                      <li><a href='https://github.com/RBirkeland'>René Birkeland</a></li>
+                      <li><a href='https://github.com/FelsyWaschbaer'>Daniel Schubert</a></li>
+                      <li><a href='https://github.com/Sandr00'>Sandro Bauer</a></li>
+                      <li><a href='https://github.com/simonzachau'>Simon Zachau</a></li>
+                      <li><a href='https://github.com/Gillian0023'>Lin Ji</a></li>
+                    </ul>
+                  </div>
+                  <div className='contributors col-md-6'>
+                    <h4>Visualization</h4>
+                    <ul>
+                      <li><a href='https://github.com/chaoran-chen'>Chaoran Chen</a></li>
+                      <li><a href='https://github.com/lustoykov'>Lyubomir Stoykov</a></li>
+                      <li><a href='https://github.com/martomi'>Martin Mihaylov</a></li>
+                      <li><a href='https://github.com/demirdagemir'>Emir Demirdag</a></li>
+                      <li><a href='https://github.com/syncall'>Markus Sosnowski</a></li>
+                      <li><a href='https://github.com/yetolas'>Panagiota Revithi</a></li>
+                      <li><a href='https://github.com/ysdevys'>Yanko Sabev</a></li>
+                      <li><a href='https://github.com/Binabiko'>Jörn von Henning</a></li>
+                    </ul>
                   </div>
                 </div>
-              </ul>
+              </div>
+            </div>
+          </div>
+          <div className='row' style={{marginTop: '20px'}}>
+            <div className='container-fluid animated fadeIn legalpage'>
+              <h1 className='text-center'>Attributions</h1>
+              <p>
+                All third party data, images and text retrieved from third
+                party sources and presented on this website were obtained in accordance with the publisher's license
+                policies.
+              </p>
+              <table className='table table-responsive' >
+                <tbody>
+                  <tr>
+                    <th>Source</th>
+                    <th>Description</th>
+                    <th>Reference</th>
+                    <th>License</th>
+                  </tr>
+                  <tr>
+                    <td>IMSLP</td>
+                    <td>
+                      IMSLP stands for the International Music Score Library Project and was started in 2006. The logo on the main page is a capital letter A. It was taken from the beginning of the very first printed book of music, the Harmonice Musices Odhecaton. It was published in Venice in 1501 by Ottaviano Petrucci, the library's namesake.
+                    </td>
+                    <td ><a href='http://imslp.org/' target='_blank'>http://imslp.org/</a>
+                    </td>
+                    <td><a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a></td>
+                  </tr>
+                  <tr>
+                    <td>DBPedia</td>
+                    <td>DBpedia is a crowd-sourced community effort to extract structured information from Wikipedia and make this information available on the Web. DBpedia allows you to ask sophisticated queries against Wikipedia, and to link the different data sets on the Web to Wikipedia data.
+                    </td>
+                    <td><a href='http://dbpedia.org/' target='_blank'>http://dbpedia.org/</a>
+                    </td>
+                    <td><a href='https://creativecommons.org/licenses/by-sa/2.0/' target='_blank'>CC-BY-SA</a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td >Worldcat</td>
+                    <td>
+                      WorldCat.org lets you search the collections of libraries in your community and thousands more around the world.
+                    </td>
+                    <td>
+                      <a href='https://www.worldcat.org/' target='_blank'>https://www.worldcat.org/</a>
+                    </td>
+                    <td>
+                      <a href='https://creativecommons.org/licenses/by-sa/2.0/' target='_blank' >CC-BY-SA</a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>MusicBrainz</td>
+                    <td>
+                      MusicBrainz is a community-maintained open source encyclopedia of music information.
+                    </td>
+                    <td><a href='https://musicbrainz.org/' target='_blank'>https://musicbrainz.org/</a>
+                    </td>
+                    <td><a href='https://creativecommons.org/licenses/by-sa/2.0/' target='_blank' >CC-BY-SA</a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>AllMusic</td>
+                    <td>
+                      AllMusic is a comprehensive and in-depth resource for finding out more about the albums, bands, musicians and songs you love.
+                    </td>
+                    <td><a href='http://www.allmusic.com/' target='_blank'>http://www.allmusic.com/</a>
+                    </td>
+                    <td><a href='https://creativecommons.org/licenses/by-sa/2.0/' target='_blank' >CC-BY-SA</a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Tom Hohendorf</td>
+                    <td>
+                      Tom Hohendorf designed our Icons and Logo.
+                    </td>
+                    <td><a href='https://github.com/manekenT' target='_blank'>https://github.com/manekenT</a>
+                    </td>
+                    <td><a href='https://creativecommons.org/licenses/by-sa/2.0/' target='_blank' >CC-BY-SA</a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
