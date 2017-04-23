19028e45c7c5af95e6a4059039d039bd354a518e
Emir Demirdag <emirdemirdag@gmail.com>
2017-04-23 22:41:19 +0200

    Add change our stats, adjust color of the logo

--- client/components/Landing.js
+++ client/components/Landing.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Searchbar from './Searchbar'
 import LandingWidgets from './static/LandingWidgets'
+import CheckOurStats from './static/CheckOurStats'
 import headerImage from '../../style/img/logo/logo-landing.svg'
 import '../../style/landing.scss'
 
@@ -11,12 +12,14 @@ const Landing = React.createClass({
         <div className='container-fluid' style={{ width: '70%', height: '60vh' }}>
           <div className='center-block text-center text-white margin-top-10-p animated fadeIn'>
             <img src={headerImage} alt='Music Connection Machine Logo' style={{maxWidth: '600px', paddingTop: '30px'}} />
+            <h3 className='margin-top-5-p' style={{color: 'rgb(51, 51, 51)'}}>Discover connections between composers, musicians and music works.</h3>
           </div>
           <div className='row margin-top-10-p'>
             <Searchbar displayHelpText />
           </div>
         </div>
         <LandingWidgets />
+        <CheckOurStats />
       </div>
     )
   }
