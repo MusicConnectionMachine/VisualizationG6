826b7dcd076f2a778b00a5bb572628aa1e5e70f4
syncall <markus.sosnowski@tum.de>
2017-04-26 14:25:40 +0200

    Improve styling on mobile devices

--- client/components/static/LandingWidgets.js
+++ client/components/static/LandingWidgets.js
@@ -7,65 +7,53 @@ import mapPhoto from '../../../style/img/map-widget.png'
 const LandingWidgets = React.createClass({
   render () {
     return (
-      <section id='widgets' className='container-fluid padding-top-bottom' style={{marginTop: '100px'}}>
-        <div className='container'>
+      <section id='widgets' className='padding-top-bottom' style={{marginTop: '100px'}}>
+        <div className='container-fluid'>
           <header className='section-header text-center'>
             <h1 className='scrollimation scale-in'>Widgets</h1>
           </header>
           <div className='row'>
-            <div className='col-sm-12'>
-              <div className='row'>
-                <h2 className='text-center'>Map</h2>
-              </div>
-              <div className='row margin-top-5-p parent-center'>
-                <div className='col-sm-5'>
-                  <p className='text-center scrollimation fade-in'>This widget presents the base for visualizing geographical locations that left a mark in the world of classical music. The widget is a work in progress and many more locations are still to be added.</p>
-                </div>
-                <div className='col-sm-7'>
-                  <img className='img-responsive img-center scrollimation fade-left' src={mapPhoto} alt='' />
-                </div>
-              </div>
-              <div className='row'>
-                <p className='text-center'><a className='btn btn-meflat' href='#'>Explore</a></p>
-              </div>
+            <h2 className='text-center'>Map</h2>
+          </div>
+          <div className='row margin-top-5-p'>
+            <div className='col-md-5'>
+              <p className='text-center scrollimation fade-in'>This widget presents the base for visualizing geographical locations that left a mark in the world of classical music. The widget is a work in progress and many more locations are still to be added.</p>
+            </div>
+            <div className='col-md-7'>
+              <img className='img-responsive img-center scrollimation fade-left' src={mapPhoto} alt='' />
             </div>
           </div>
           <div className='row'>
-            <div className='col-sm-12'>
-              <div className='row'>
-                <h2 className='text-center'>Relations</h2>
-              </div>
-              <div className='row margin-top-5-p parent-center'>
-                <div className='col-sm-5'>
-                  <img className='img-responsive img-center scrollimation fade-left' src={relationPhoto1} alt='' />
-                  <img className='img-responsive img-center scrollimation fade-left' src={relationPhoto2} alt='' />
-                </div>
-                <div className='col-sm-7'>
-                  <p className='text-center scrollimation fade-in'>Seamlessly navigate through millions of connections between composers, musicians and music works.</p>
-                </div>
-              </div>
-              <div className='row'>
-                <p className='text-center'><a className='btn btn-meflat' href='#'>Explore</a></p>
-              </div>
+            <p className='text-center'><a className='btn btn-meflat' href='#'>Explore</a></p>
+          </div>
+          <div className='row'>
+            <h2 className='text-center'>Relations</h2>
+          </div>
+          <div className='row margin-top-5-p'>
+            <div className='col-md-7'>
+              <img className='img-responsive img-center scrollimation fade-left' src={relationPhoto1} alt='' />
+              <img className='img-responsive img-center scrollimation fade-left' src={relationPhoto2} alt='' />
+            </div>
+            <div className='col-md-5'>
+              <p className='text-center scrollimation fade-in'>Seamlessly navigate through millions of connections between composers, musicians and music works.</p>
             </div>
           </div>
           <div className='row'>
-            <div className='col-sm-12'>
-              <div className='row'>
-                <h2 className='text-center'>Timeline</h2>
-              </div>
-              <div className='row margin-top-5-p parent-center'>
-                <div className='col-sm-5'>
-                  <p className='text-center scrollimation fade-in'>Scroll through the timeline and see what internet knows about the history of music.</p>
-                </div>
-                <div className='col-sm-7'>
-                  <img className='img-responsive img-center scrollimation fade-left' src={timelinePhoto} alt='' />
-                </div>
-              </div>
-              <div className='row'>
-                <p className='text-center'><a className='btn btn-meflat' href='#'>Explore</a></p>
-              </div>
+            <p className='text-center'><a className='btn btn-meflat' href='#'>Explore</a></p>
+          </div>
+          <div className='row'>
+            <h2 className='text-center'>Timeline</h2>
+          </div>
+          <div className='row margin-top-5-p'>
+            <div className='col-md-5'>
+              <p className='text-center scrollimation fade-in'>Scroll through the timeline and see what internet knows about the history of music.</p>
             </div>
+            <div className='col-md-7'>
+              <img className='img-responsive img-center scrollimation fade-left' src={timelinePhoto} alt='' />
+            </div>
+          </div>
+          <div className='row'>
+            <p className='text-center'><a className='btn btn-meflat' href='#'>Explore</a></p>
           </div>
         </div>
       </section>
